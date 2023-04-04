ed56f6a1fc719a5a3308754a1867598998494088
Leandro Bermeo <leandrofbermeo@gmail.com>
2023-04-04 18:23:36 +0100

    fix paths for stained glasses

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,54 +1,70 @@
 
+// Ring Bounce
+let ring = gsap.timeline()
+ring.from('.ring__svg', {
+  opacity: 0,
+  y: -200,
+  ease: "bounce.out",
+  scale: 2,
+  duration:1.2,
+})
+
+
+
+let std_stroke = gsap.timeline();
+
+
+
 
 
 
 // Text Slide in
-let std_text = gsap.timeline({
-  opacity: 0,
-})
-std_text.set('.std-text path', {
-  opacity: 0,
-  x: -1000,
-})
-std_text.to('.std-text path', {
-  opacity: 1,
-  duration: 1,
-  x: 0,
-  ease: "Bounce.easeOut",
-  delay: "random(0, 1)",
-})
+// let std_text = gsap.timeline({
+//   opacity: 0,
+// })
+// std_text.set('.std-text path', {
+//   opacity: 0,
+//   x: -1000,
+// })
+// std_text.to('.std-text path', {
+//   opacity: 1,
+//   duration: 1,
+//   x: 0,
+//   ease: "Bounce.easeOut",
+//   delay: "random(0, 1)",
+// })
 
 
 
 // Blink Effect
 
-let std_blink = gsap.timeline({
-  repeat: -1,
-  yoyo: true,
-})
-std_blink.to('.std-sparkles path', {
-  opacity: 0,
-  stagger: "random(0, .5)",
-  delay: "random(0, 1)",
-})
+// let std_blink = gsap.timeline({
+//   repeat: -1,
+//   yoyo: true,
+// })
+// std_blink.to('.std-sparkles path', {
+//   opacity: 0,
+//   stagger: "random(0, .5)",
+//   delay: "random(0, 1)",
+// })
 
 
-// Save the Date SVG FLOAT
-let std__svg = gsap.timeline({
-  repeat:-1,
-  yoyo: true,
+// // Save the Date SVG FLOAT
+// let std__svg = gsap.timeline({
+//   repeat:-1,
+//   yoyo: true,
 
-})
+// })
 
-std__svg.to('.std__svg', {
-  duration: .35,
-  opacity: 1,
-  ease: "bounce.easeIn",
-  scale:1.025,
-}, '+=3').to('.std__svg' , {
-  rotation: 10,
+// std__svg.to('.std__svg', {
+//   duration: .35,
+//   opacity: 1,
+//   ease: "bounce.easeIn",
+//   scale:1.025,
+// }, '+=3').to('.std__svg' , {
+//   rotation: 10,
 
-},'+=1').to('.std__svg' , {
-  rotation:-10,
-})
+// },'+=1').to('.std__svg' , {
+//   rotation:-10,
+// })
 
--- main/index.f4cb73f5.js
+++ main/index.f4cb73f5.js
@@ -1,42 +1,51 @@
-// Text Slide in
-let std_text = gsap.timeline({
-    opacity: 0
-});
-std_text.set(".std-text path", {
+// Ring Bounce
+let ring = gsap.timeline();
+ring.from(".ring__svg", {
     opacity: 0,
-    x: -1000
+    y: -200,
+    ease: "bounce.out",
+    scale: 2,
+    duration: 1.2
 });
-std_text.to(".std-text path", {
-    opacity: 1,
-    duration: 1,
-    x: 0,
-    ease: "Bounce.easeOut",
-    delay: "random(0, 1)"
-});
-// Blink Effect
-let std_blink = gsap.timeline({
-    repeat: -1,
-    yoyo: true
-});
-std_blink.to(".std-sparkles path", {
-    opacity: 0,
-    stagger: "random(0, .5)",
-    delay: "random(0, 1)"
-});
-// Save the Date SVG FLOAT
-let std__svg = gsap.timeline({
-    repeat: -1,
-    yoyo: true
-});
-std__svg.to(".std__svg", {
-    duration: .35,
-    opacity: 1,
-    ease: "bounce.easeIn",
-    scale: 1.025
-}, "+=3").to(".std__svg", {
-    rotation: 10
-}, "+=1").to(".std__svg", {
-    rotation: -10
-});
+let std_stroke = gsap.timeline(); // Text Slide in
+ // let std_text = gsap.timeline({
+ //   opacity: 0,
+ // })
+ // std_text.set('.std-text path', {
+ //   opacity: 0,
+ //   x: -1000,
+ // })
+ // std_text.to('.std-text path', {
+ //   opacity: 1,
+ //   duration: 1,
+ //   x: 0,
+ //   ease: "Bounce.easeOut",
+ //   delay: "random(0, 1)",
+ // })
+ // Blink Effect
+ // let std_blink = gsap.timeline({
+ //   repeat: -1,
+ //   yoyo: true,
+ // })
+ // std_blink.to('.std-sparkles path', {
+ //   opacity: 0,
+ //   stagger: "random(0, .5)",
+ //   delay: "random(0, 1)",
+ // })
+ // // Save the Date SVG FLOAT
+ // let std__svg = gsap.timeline({
+ //   repeat:-1,
+ //   yoyo: true,
+ // })
+ // std__svg.to('.std__svg', {
+ //   duration: .35,
+ //   opacity: 1,
+ //   ease: "bounce.easeIn",
+ //   scale:1.025,
+ // }, '+=3').to('.std__svg' , {
+ //   rotation: 10,
+ // },'+=1').to('.std__svg' , {
+ //   rotation:-10,
+ // })
 
 //# sourceMappingURL=index.f4cb73f5.js.map
